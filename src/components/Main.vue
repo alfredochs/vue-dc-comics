1551a8044aa86182a3c6593196c23032b66595bd
alfredochs <chanta.alfredo@gmail.com>
2021-11-12 17:42:17 +0100

    jumbotron applicato

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,39 +1,55 @@
 <template>
   <div class="main-container">
-    <div class="top-black">Inserire qui il contenuto</div>
+    <div class="top-black">
+      <CardsSection></CardsSection>
+    </div>
     <div class="bottom-blue">
       <div class="flex">
-        <div class="card-container">
-          <img src="../assets/buy-comics-digital-comics.png" alt="" />
-          <span>Digital Comics</span>
-        </div>
-
-        <div class="card-container">
-          <img src="../assets/buy-comics-merchandise.png" alt="" />
-          <span>Digital Comics</span>
-        </div>
-
-        <div class="card-container">
-          <img src="../assets/buy-comics-subscriptions.png" alt="" />
-          <span>Digital Comics</span>
-        </div>
-
-        <div class="card-container">
-          <img src="../assets/buy-comics-shop-locator.png" alt="" />
-          <span>Digital Comics</span>
-        </div>
-
-        <div class="card-container">
-          <img src="../assets/buy-dc-power-visa.svg" alt="" />
-          <span>Digital Comics</span>
+        <div
+          class="card-container"
+          v-for="(chiave, i) in imgBlueContainer"
+          :key="i"
+        >
+          <img :src="chiave.src" alt="" />
+          <span>{{ chiave.description }}</span>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
+// import cardsContainer from "@/components/CardsSection.vue";
+import CardsSection from "../components/CardsSection.vue";
 export default {
+  components: { CardsSection },
   name: "Main",
+  data() {
+    return {
+      imgBlueContainer: [
+        {
+          src: require("../assets/buy-comics-digital-comics.png"),
+          description: "DIGITAL COMICS",
+        },
+        {
+          src: require("@/assets/buy-comics-merchandise.png"),
+          description: "DC MERCHANDISE",
+        },
+        {
+          src: require("../assets/buy-comics-subscriptions.png"),
+          description: "SUBSCRIPTION",
+        },
+        {
+          src: require("../assets/buy-comics-shop-locator.png"),
+          description: "COMIC SHOP LOCATOR",
+        },
+        {
+          src: require("../assets/buy-dc-power-visa.svg"),
+          description: "DC POWER VISA",
+        },
+      ],
+    };
+  },
 };
 </script>
 
